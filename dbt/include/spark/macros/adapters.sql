ca69a97809822a576a9a2668ef2eb4010390c96e
cadl <ctrlaltdeleteliu@gmail.com>
2022-08-25 17:40:31 +0800

    fix: build error primarykey

--- dbt/include/spark/macros/adapters.sql
+++ dbt/include/spark/macros/adapters.sql
@@ -31,7 +31,8 @@
   {%- set options = config.get('options') -%}
   {%- if config.get('file_format') == 'hudi' -%}
     {%- set unique_key = config.get('unique_key') -%}
-    {%- set expected_options_primarykey = unique_key|join(',') if unique_key is sequence else unique_key -%}
+    {%- set expected_options_primarykey = unique_key|join(',') if unique_key is sequence and (unique_key is not string and unique_key is not mapping) else unique_key -%}
+
     {%- if unique_key is not none and options is none -%}
       {%- set options = {'primaryKey': expected_options_primarykey} -%}
     {%- elif unique_key is not none and options is not none and 'primaryKey' not in options -%}
